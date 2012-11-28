02ecef58cbb09bdedcb429f5410b6119137089c8
Jackopo <jacopo.ghisolfi@gmail.com>
2012-11-28 13:46:08 +0100

    User_id in Posts is now validated and not accessible outside anymore

--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ActiveRecord::Base
-  attr_accessible :content, :user_id
+  attr_accessible :content
   belongs_to :user
+
+  validates :user_id, presence: true
+  default_scope order: 'posts.created_at DESC'
 end
