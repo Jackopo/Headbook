e94cd7a4bcc5c93641bb7d6e83a829bcea31a797
Torsten Rüter <torsten@rueter.net>
2013-01-11 16:41:06 +0100

    valid attribute for rspec test case: postscontroller

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -32,9 +32,12 @@ Headbook::Application.configure do
   # Do not compress assets
   config.assets.compress = false
 
+  config.assets.compile = true
+
+  # Expands the lines which load the assets
+  config.assets.debug = false
+
   # Needed for devise
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
 
-  # Expands the lines which load the assets
-  config.assets.debug = true
 end
--- spec/controllers/posts_controller_spec.rb
+++ spec/controllers/posts_controller_spec.rb
@@ -24,7 +24,10 @@ describe PostsController do
   # Post. As you add validations to Post, be sure to
   # update the return value of this method accordingly.
   def valid_attributes
-    {  }
+    { 
+      :content => "Valid Content", 
+      :user_id => 1
+    }
   end
 
   # This should return the minimal set of values that should be in the session
