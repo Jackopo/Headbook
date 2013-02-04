0d4be9ce7e798c5b8c964baf7b617462aef70426
Torsten Rüter <torsten@rueter.net>
2013-02-04 13:25:15 +0100

    ..?

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121214003453) do
+ActiveRecord::Schema.define(:version => 20130124181637) do
 
   create_table "friendships", :force => true do |t|
     t.integer  "first_person_id"
@@ -41,6 +41,7 @@ ActiveRecord::Schema.define(:version => 20121214003453) do
     t.datetime "last_sign_in_at"
     t.string   "current_sign_in_ip"
     t.string   "last_sign_in_ip"
+    t.boolean  "admin"
   end
 
   add_index "users", ["email"], :name => "index_users_on_email", :unique => true
